72de448e209dfdc72252b277c3dd644f94f713cd
Shayokh <shayokhshuvro14@gmail.com>
2024-09-10 07:55:59 +0600

    define `Lexer` class and `nextChar()` method

--- lib/lexer/lexer.cpp
+++ lib/lexer/lexer.cpp
@@ -1,5 +1,5 @@
-#ifndef LEX_H
-#define LEX_H
+#ifndef LEXER_H
+#define LEXER_H
 
 #include <string>
 
@@ -32,6 +32,38 @@ struct Token
     std::string text; // Holds the literal value of the token
 
     Token(std::string text, TokenType type) : text(text), type(type) {}
+};
+
+class Lexer
+{
+public:
+    char curChar;       // Current character in the source string.
+    int curPos = -1;    // Current position in the source string.
+    std::string source; // Source code to lexer as a string.
+
+    // curChar is set to the first char in the source string and curPos is set to 0
+    // when nextChar() is called for the first time.
+
+    // Constructor
+    Lexer(const std::string &source) : source(source)
+    {
+        nextChar();
+    };
+
+    // Process the next character.
+    void nextChar()
+    {
+        curPos += 1;
+
+        if (curPos >= source.length())
+        {
+            curChar = '\0'; // EOF
+        }
+        else
+        {
+            curChar = source[curPos];
+        }
+    };
 };
 
 #endif
